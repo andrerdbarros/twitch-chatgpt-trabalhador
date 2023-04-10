c6731ec0b35810d58359127280fdea2d913ba331
andrerdbarros <52177461+andrerdbarros@users.noreply.github.com>
2023-04-10 19:36:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.get('/gpt/:text', async (req, res) => {
         model: "gpt-3.5-turbo",
         messages: messages,
         temperature: 0.5,
-        max_tokens: 120,
+        max_tokens: 128,
         top_p: 1,
         frequency_penalty: 0,
         presence_penalty: 0,
@@ -88,11 +88,11 @@ app.get('/gpt/:text', async (req, res) => {
         messages.push({role: "assistant", content: agent_response})
 
         //Check for Twitch max. chat message length limit and slice if needed
-        if(agent_response.length > 1399){
-          console.log("Agent answer exceeds twitch chat limit. Slicing to first 399 characters.")
-          agent_response = agent_response.substring(0, 1399)
-          console.log ("Sliced agent answer: " + agent_response)
-        }
+        //if(agent_response.length > 1399){
+        //  console.log("Agent answer exceeds twitch chat limit. Slicing to first 399 characters.")
+        //  agent_response = agent_response.substring(0, 1399)
+        //  console.log ("Sliced agent answer: " + agent_response)
+        //}
 
         res.send(agent_response)
       } else {
@@ -108,7 +108,7 @@ app.get('/gpt/:text', async (req, res) => {
         model: "text-davinci-003",
         prompt: prompt,
         temperature: 0.5,
-        max_tokens: 120,
+        max_tokens: 128,
         top_p: 1,
         frequency_penalty: 0,
         presence_penalty: 0,
@@ -117,11 +117,11 @@ app.get('/gpt/:text', async (req, res) => {
         let agent_response = response.data.choices[0].text
           console.log ("Agent answer: " + agent_response)
           //Check for Twitch max. chat message length limit and slice if needed
-          if(agent_response.length > 1399){
-            console.log("Agent answer exceeds twitch chat limit. Slicing to first 399 characters.")
-            agent_response = agent_response.substring(0, 1399)
-            console.log ("Sliced Agent answer: " + agent_response)
-          }
+          //if(agent_response.length > 1399){
+          //  console.log("Agent answer exceeds twitch chat limit. Slicing to first 399 characters.")
+          //  agent_response = agent_response.substring(0, 1399)
+          //  console.log ("Sliced Agent answer: " + agent_response)
+          //}
 
           res.send(agent_response)
       } else {
