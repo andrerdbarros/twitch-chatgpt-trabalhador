72e336bd583d5fdde1579af079117288b49c0a09
andrerdbarros <52177461+andrerdbarros@users.noreply.github.com>
2023-04-11 00:59:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -169,23 +169,7 @@ app.get('/image/:text', async (req, res) => {
 		size: "1024x1024",
       });
     
-      if (response.data.choices) {
-        let agent_response = response['data'][0]['url']
-
-        console.log ("Agent answer: " + agent_response)
-        messages.push({role: "assistant", content: agent_response})
-
-        //Check for Twitch max. chat message length limit and slice if needed
-        //if(agent_response.length > 1399){
-        //  console.log("Agent answer exceeds twitch chat limit. Slicing to first 399 characters.")
-        //  agent_response = agent_response.substring(0, 1399)
-        //  console.log ("Sliced agent answer: " + agent_response)
-        //}
-
-        res.send(agent_response)
-      } else {
-        res.send("Something went wrong. Try again later!")
-      }
+      res.send(response.data.data[0].url);
 
 
     
