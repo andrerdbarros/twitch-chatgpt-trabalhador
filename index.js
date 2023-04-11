4b7bc24132a8393e5c0839fa5d2076043233751e
andrerdbarros <52177461+andrerdbarros@users.noreply.github.com>
2023-04-11 00:53:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ const GPT_MODE = process.env.GPT_MODE
 
 let file_context = "You are a helpful Twitch Chatbot."
 
-
-
 const messages = [
   {role: "system", content: "You are a helpful Twitch Chatbot."}
 ];
@@ -20,6 +18,8 @@ console.log("OpenAI API Key:" + process.env.OPENAI_API_KEY)
 
 app.use(express.json({extended: true, limit: '1mb'}))
 
+
+ 
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
@@ -44,14 +44,11 @@ if (process.env.GPT_MODE === "CHAT"){
 
 }
 
-app.get('/img/:text', async (req, res) => {
+app.get('/gpt/:text', async (req, res) => {
     
     //The agent should recieve Username:Message in the text to identify conversations with different users in his history. 
     
     const text = req.params.text
-    
-    console.log("TEXT = " + text)
-    
     const { Configuration, OpenAIApi } = require("openai");
 
     const configuration = new Configuration({
@@ -133,6 +130,64 @@ app.get('/img/:text', async (req, res) => {
           res.send("Something went wrong. Try again later!")
       }
     }
+    
+})
+
+
+
+app.get('/image/:text', async (req, res) => {
+    
+    //The agent should recieve Username:Message in the text to identify conversations with different users in his history. 
+    
+    const text = req.params.text
+    const { Configuration, OpenAIApi } = require("openai");
+
+    const configuration = new Configuration({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+
+    const openai = new OpenAIApi(configuration);      
+    
+      //CHAT MODE EXECUTION
+
+      //Add user message to  messages
+      messages.push({role: "user", content: text})
+      //Check if message history is exceeded
+      console.log("Conversations in History: " + ((messages.length / 2) -1) + "/" + process.env.HISTORY_LENGTH)
+      if(messages.length > ((process.env.HISTORY_LENGTH * 2) + 1)) {
+          console.log('Message amount in history exceeded. Removing oldest user and agent messages.')
+          messages.splice(1,2)
+     }
+    
+      console.log("Messages: ")
+      console.dir(messages)
+      console.log("User Input: " + text)
+
+      const response = await openai.createImage({
+        prompt: prompt,
+		n: 1,
+		size: "1024x1024",
+      });
+    
+      if (response.data.choices) {
+        let agent_response = response['data'][0]['url']
+
+        console.log ("Agent answer: " + agent_response)
+        messages.push({role: "assistant", content: agent_response})
+
+        //Check for Twitch max. chat message length limit and slice if needed
+        //if(agent_response.length > 1399){
+        //  console.log("Agent answer exceeds twitch chat limit. Slicing to first 399 characters.")
+        //  agent_response = agent_response.substring(0, 1399)
+        //  console.log ("Sliced agent answer: " + agent_response)
+        //}
+
+        res.send(agent_response)
+      } else {
+        res.send("Something went wrong. Try again later!")
+      }
+
+
     
 })
 app.listen(process.env.PORT || 3000)
